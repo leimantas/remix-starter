1e7f503dc72b189754f9ce5847f3f76d93d1e25c
Eimantas <eimantas.lapinskas@gmail.com>
2023-08-28 22:50:05 +0200

    loader + component

--- app/routes/_public+/about.tsx
+++ app/routes/_public+/about.tsx
@@ -1,3 +1,25 @@
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+
+export const loader = async () => {
+  return json({ name: "Remix", age: 1 });
+};
+
 export default function AboutPage() {
-  return <h1 className="text-2xl text-center my-4">About page</h1>;
+  const data = useLoaderData<typeof loader>();
+
+  return (
+    <div className="max-w-sm mx-auto">
+      <h1 className="text-2xl text-center my-4">About page</h1>
+
+      <div>
+        <div>Name: {data.name}</div>
+        <div>Age: {data.age}</div>
+      </div>
+
+      <pre className="p-4 mt-4 bg-slate-100">
+        {JSON.stringify(data, null, 2)}
+      </pre>
+    </div>
+  );
 }
